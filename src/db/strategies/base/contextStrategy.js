0658ae6da2286d3ede25a911a3dc160ae70db074
youssef-md <emaildeyoussefmuhamad@gmail.com>
2019-07-26 00:11:52 -0300

    Add the base strategy

---
+++ src/db/strategies/base/contextStrategy.js
@@ -0,0 +1,15 @@
+const iCrud = require('../interfaces/interfaceCrud')
+
+class ContextStrategy extends iCrud {
+  constructor(strategy) {
+    super()
+    this._database = strategy
+  }
+
+  create(item) { return this._database.create(item) }
+  read(item) { return this._database.read(item) }
+  update(id, item) { return this._database.update(id, item) }
+  delete(id) { return this._database.delete(id) }
+}
+
+module.exports = ContextStrategy
