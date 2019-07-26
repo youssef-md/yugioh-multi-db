83b3c1f110da60c871aceefc389e14fd08c420a2
youssef-md <emaildeyoussefmuhamad@gmail.com>
2019-07-26 00:09:33 -0300

    Start to stucture the project by adding the interfaceCrud

---
+++ src/db/strategies/interfaces/interfaceCrud.js
@@ -0,0 +1,12 @@
+class NotImplementedException extends Error {
+  constructor() { super("Not Implemented Exception!") }
+}
+
+class iCrud { // if an extending class didn't implement one of these methods throw an exception
+  create(item) { throw new NotImplementedException() }
+  read(query) { throw new NotImplementedException() }
+  update(id, item) { throw new NotImplementedException() }
+  delete(id) { throw new NotImplementedException() }
+}
+
+module.exports = iCRUD
