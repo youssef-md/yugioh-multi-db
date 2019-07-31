ae5027443e8306d9f280e2d1d85d5e0657985219
youssef-md <emaildeyoussefmuhamad@gmail.com>
2019-07-31 00:55:13 -0300

    Generalizing mongodb strategy for supporting multi-schemas

--- src/db/strategies/mongodb/mongodb.js
+++ src/db/strategies/mongodb/mongodb.js
@@ -1,19 +1,16 @@
 const iCrud = require('../interfaces/interfaceCrud')
 const Mongoose = require('mongoose')
-const Card = require('../../models/CardMongo')
 
 const { MONGODB_USERNAME, MONGODB_PASSWORD } = require('../../../dbLogin')
 const connectionAddress = `mongodb://${MONGODB_USERNAME}:${MONGODB_PASSWORD}@localhost:27017/yugioh`
 const STATUS = { 0: 'Disconnected', 1: 'Connected', 2: 'Connecting', 3: 'Disconnecting' }
 
 class MongoDB extends iCrud {
-  constructor() {
+  constructor(connection, schema) {
     super()
-    this._connection = null
-    this._YuGiOh = null
+    this._connection = connection
+    this._schema = schema
   }
-
-  async defineMondel() { this._YuGiOh = Mongoose.model('CARD', Card) }
 
   async isConnected() {
     const state = STATUS[this._connection.readyState]
@@ -22,34 +19,35 @@ class MongoDB extends iCrud {
     return STATUS[this._connection.readyState] // check again if is connected
   }
 
-  async connect() {
-    Mongoose.connect(connectionAddress, { useNewUrlParser: true }, function (error) {
+  static connect() {
+    Mongoose.connect(connectionAddress, { useNewUrlParser: true }, error => {
       if (!error) return
-      console.log('Connection to MongoDB Failed!', error)
+      console.log('Connection with MongoDB Failed!', error)
     })
-    this._connection = Mongoose.connection
-    this._connection.once('open', () => console.log('Mongodb Database running'))
-    this.defineMondel()
+
+    const connection = Mongoose.connection
+    connection.once('open', () => console.log('Mongodb is running...'))
+    return connection
   }
 
   async create(item) {
     console.log(`Creating the item ${item.name} in MongoDB...`)
-    return await this._YuGiOh.create(item)
+    return await this._schema.create(item)
   }
 
   read(query, skip = 0, limit = 0) { // with pagination
     console.log(`Reading the query ${query} in MongoDB...`)
-    return this._YuGiOh.find(query).skip(skip).limit(limit)
+    return this._schema.find(query).skip(skip).limit(limit)
   }
 
   update(id, item) {
     console.log(`Updating the item with id ${id} in MongoDB...`)
-    return this._YuGiOh.updateOne({ _id: id }, { $set: item })
+    return this._schema.updateOne({ _id: id }, { $set: item })
   }
 
   delete(id) {
     console.log(`Deleting the item with id ${id} in MongoDB...`)
-    return this._YuGiOh.deleteOne({ _id: id })
+    return this._schema.deleteOne({ _id: id })
   }
 }
 
