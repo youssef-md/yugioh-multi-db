600ec99c2f486a7462a188fd1a9ba0a3dad6c01f
youssef-md <emaildeyoussefmuhamad@gmail.com>
2019-08-01 01:33:22 -0300

    Example using Hapi.js to return all the cards from mongodb via GET

---
+++ src/api-example.js
@@ -0,0 +1,28 @@
+const Hapi = require('hapi')
+const Context = require('./db/strategies/base/contextStrategy')
+const Mongodb = require('./db/strategies/mongodb/mongodb')
+const yugiohSchema = require('./db/strategies/mongodb/schemas/yugiohSchema')
+
+const app = new Hapi.Server({
+  port: 5000
+})
+
+main()
+async function main() {
+
+  const connection = Mongodb.connect()
+  const context = new Context(new Mongodb(connection, yugiohSchema))
+
+  app.route([
+    {
+      path: '/cards',
+      method: 'GET',
+      handler: (req, head) => {
+        return context.read()
+      }
+    }
+  ])
+
+  await app.start()
+  console.log('Server is running at', app.info.port)
+}
