94fc38b36294325004d8341c01af8516db75e39f
youssef-md <emaildeyoussefmuhamad@gmail.com>
2019-08-11 22:34:11 -0300

    Add test for obtaining a JWT token

---
+++ src/tests/authCards.test.js
@@ -0,0 +1,27 @@
+const { deepEqual, ok } = require('assert')
+const api = require('../api')
+
+let app = {}
+
+describe('Auth test suite', function () {
+  this.beforeAll(async () => {
+    app = await api
+  })
+
+  it('Should obtain a token', async () => {
+    const res = await app.inject({
+      method: 'POST',
+      url: '/login',
+      payload: {
+        username: 'josedoegito',
+        password: '123123'
+      }
+    })
+
+    const statusCode = res.statusCode
+    const data = JSON.parse(res.payload)
+
+    deepEqual(statusCode, 200)
+    ok(data.token.lentgh > 10)
+  })
+})
