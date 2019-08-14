06d59dab9d0f94f3e6432e2ba56f46a4e7f94334
youssef-md <emaildeyoussefmuhamad@gmail.com>
2019-08-13 23:39:08 -0300

    lib bcrypt and initial test for password helper

---
+++ src/tests/passwordHelper.test.js
@@ -0,0 +1,9 @@
+const { deepEqual, ok } = require('assert')
+const PASSWORD = 'youssef@123'
+
+describe.only('User Helper test suite', function () {
+  it('Should generate a hash with a password', async () => {
+    const res = ''
+    ok(res.lentgh > 10)
+  })
+})
